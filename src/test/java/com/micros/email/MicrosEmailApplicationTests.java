7ac830a725da1bb8199268b2a01ff3ddb3361c9c
Guilherme Fernandes Vidal <guileads@gmail.com>
2024-02-01 18:47:42 -0300

    setup project and docker

---
+++ src/test/java/com/micros/email/MicrosEmailApplicationTests.java
@@ -0,0 +1,13 @@
+package com.micros.email;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicrosEmailApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
